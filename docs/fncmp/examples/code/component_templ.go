9c908931f7f13220ba25e2551cf67bb90a3dbb82
Sean Burman <sean.burman@icloud.com>
2024-03-08 23:18:55 +0000

    Updated fncmp docs

--- docs/fncmp/examples/code/component_templ.go
+++ docs/fncmp/examples/code/component_templ.go
@@ -93,3 +93,45 @@ func Greeting(who string) templ.Component {
 		return templ_7745c5c3_Err
 	})
 }
+
+func Index() templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
+		if !templ_7745c5c3_IsBuffer {
+			templ_7745c5c3_Buffer = templ.GetBuffer()
+			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var7 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var7 == nil {
+			templ_7745c5c3_Var7 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html><head><title>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var8 := `My Project`
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var8)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title><script src=\"fncmp.min.js\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var9 := ``
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var9)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</script></head><body><main></main></body></html>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if !templ_7745c5c3_IsBuffer {
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteTo(templ_7745c5c3_W)
+		}
+		return templ_7745c5c3_Err
+	})
+}
--- docs/fncmp/examples/code/main.go
+++ docs/fncmp/examples/code/main.go
@@ -19,21 +19,55 @@ func HandleIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/index.html")
 }
 
-func handleClick(context.Context) fncmp.FnComponent {
-	return fncmp.NewFn(Greeting("Sean"))
+func HandleTest(w http.ResponseWriter, r *http.Request) {
+	c := Index()
+	c.Render(r.Context(), w)
 }
 
-var button = fncmp.NewFn(Button("Click me!"))
+func HandleTestFn(ctx context.Context) fncmp.FnComponent {
+	item := fncmp.HTML("<h1>Test</h1>")
+	return fncmp.NewFn(ctx, item)
+}
+
+type Task struct {
+	Name     string `json:"name"`
+	Complete bool   `json:"complete"`
+}
+
+func handleSubmit(ctx context.Context) fncmp.FnComponent {
+	task, err := fncmp.EventData[Task](ctx)
+	if err != nil {
+		return fncmp.NewFn(ctx, nil)
+	}
+
+	cache, err := fncmp.UseCache[[]Task](ctx, "tasks")
+	if err != nil {
+		return fncmp.FnErr(ctx, err)
+	}
+	tasks := cache.Value()
+	tasks = append(tasks, task)
+	cache.Set(tasks)
+
+	item := fncmp.HTML("<li>" + task.Name + "</li>")
+	fn := fncmp.NewFn(ctx, item)
+	return fn.AppendElement("tasks")
+}
 
 func HandleMainFn(ctx context.Context) fncmp.FnComponent {
-	return button.
-		WithContext(ctx).
-		WithEvents(handleClick, fncmp.OnClick)
+	form := fncmp.HTML(`
+		<form>
+			<input id='name' name='name' type='text' required/>
+			<button type='submit'>Add Task</button>
+		</form>
+	`)
+	fn := fncmp.NewFn(ctx, form).WithEvents(handleSubmit, fncmp.OnSubmit)
+	return fn.AppendTag("main")
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("static")))
 	mux.Handle("/main", fncmp.MiddleWareFn(HandleIndex, HandleMainFn))
+	mux.Handle("/test", fncmp.MiddleWareFn(HandleTest, HandleTestFn))
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
